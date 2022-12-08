c8373b9b9c60a2fd568a076dc082a6b183d25ce6
Saraswathi <72403496+saraswathi2000@users.noreply.github.com>
2022-12-08 19:30:05 +0530

    Weather Observation Station 1

---
+++ BASIC SELECT/Weather Observation Station 1.sql
@@ -0,0 +1,18 @@
+/* 
+Query a list of CITY and STATE from the STATION table.
+
+The STATION table is described as follows:
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+ */
+ 
+ SELECT CITY, STATE FROM STATION;
+ 
+ 
