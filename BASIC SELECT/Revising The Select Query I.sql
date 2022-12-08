d125393623469223b5e3b3b81b259b4a1fceb39a
Saraswathi <72403496+saraswathi2000@users.noreply.github.com>
2022-12-08 18:17:22 +0530

    Revising the select query I

--- BASIC SELECT/Revising The Select Query I.sql
+++ BASIC SELECT/Revising The Select Query I.sql
@@ -1,8 +1,19 @@
--- Q) Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
-The CITY table is described as follows:
- 
 
-SOLUTION
+ /* The CITY table is described as follows: 
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | int(11)  |
+| Name        | char(35) |
+| CountryCode | char(3)  |
+| District    | char(20) |
+| Population  | int(11)  |
++-------------+----------+
+Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA. */
+
+
+SELECT * FROM CITY 
+WHERE COUNTRYCODE = 'USA' AND POPULATION >100000;
 
 
 
