ac4a17f34f836010158c5bd4c3f66745aabb9e6e
Saraswathi <72403496+saraswathi2000@users.noreply.github.com>
2022-12-08 21:44:14 +0530

    weather observation station 9

---
+++ BASIC SELECT/Weather Observation Station 9.sql
@@ -0,0 +1,17 @@
+/* The STATION table is described as follows:
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates. */
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE LEFT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
