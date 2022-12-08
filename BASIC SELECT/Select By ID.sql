b1916e9fe17bdc4544d6e669e2e1f94467489bed
Saraswathi <72403496+saraswathi2000@users.noreply.github.com>
2022-12-08 18:27:37 +0530

    Select By ID

---
+++ BASIC SELECT/Select By ID.sql
@@ -0,0 +1,16 @@
+/*
+Query all columns for a city in CITY with the ID 1661.
+
+The CITY table is described as follows: 
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | int(11)  |
+| Name        | char(35) |
+| CountryCode | char(3)  |
+| District    | char(20) |
+| Population  | int(11)  |
++-------------+----------+
+ */
+
+SELECT * FROM CITY WHERE ID = 1661;
