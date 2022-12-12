4859bb7fee22c1b9ecbc068d8a25f86c99462ea3
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-12 14:25:19 +0530

    Revising Aggregations - The count Function

---
+++ Aggregation/Revising Aggregations-The Count Function.sql
@@ -0,0 +1,15 @@
+/* The CITY table is described as follows: 
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | int(11)  |
+| Name        | char(35) |
+| CountryCode | char(3)  |
+| District    | char(20) |
+| Population  | int(11)  |
++-------------+----------+
+Query a count of the number of cities in CITY having a Population larger than 100,000. */
+
+
+SELECT COUNT(NAME) FROM CITY 
+WHERE POPULATION >100000;
