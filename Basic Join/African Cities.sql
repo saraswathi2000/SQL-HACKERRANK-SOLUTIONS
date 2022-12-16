edbd4c8970434638687815e029af96ece6f54ee6
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-16 11:34:51 +0530

    African Cities

---
+++ Basic Join/African Cities.sql
@@ -0,0 +1,6 @@
+/* Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'. */
+
+SELECT CITY.NAME
+FROM CITY
+INNER JOIN COUNTRY ON CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE COUNTRY.CONTINENT = 'Africa';
