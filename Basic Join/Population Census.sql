92b2c1c69045aa05ce118734a19fc0859a1231ea
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2023-01-02 19:38:00 +0530

    Update Population Census.sql

--- Basic Join/Population Census.sql
+++ Basic Join/Population Census.sql
@@ -1,7 +1,7 @@
 /*
 Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
 
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns in the table.*/
 
 SELECT SUM(CITY.POPULATION)
 FROM CITY
