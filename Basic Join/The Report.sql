b3000bb9bb2b07ff843e85c1ece5093df52e0a7a
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2023-01-02 19:38:33 +0530

    Update The Report.sql

--- Basic Join/The Report.sql
+++ Basic Join/The Report.sql
@@ -1,7 +1,7 @@
 /* Ketty gives Eve a task to generate a report containing three columns: 
 Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8.
 The report must be in descending order by grade -- i.e. higher grades are entered first. 
-If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. 
+If there are more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. 
 Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order.
 If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
 Write a sql query to help Eve. */
