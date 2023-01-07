152c22b685a8b6b2c2c1112e2888c837b5d6e549
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2023-01-07 11:22:37 +0530

    Update Select By ID.sql

--- BASIC SELECT/Select By ID.sql
+++ BASIC SELECT/Select By ID.sql
@@ -1,7 +1,7 @@
 /*
 Query all columns for a city in CITY with the ID 1661.
 
-The CITY table is described as follows: 
+The CITY table is described as Below: 
 +-------------+----------+
 | Field       | Type     |
 +-------------+----------+
