e392d0b0816f5e6f731899783db8533274f6877b
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2023-01-11 22:40:02 +0530

    Update Interviews.sql

--- Advanced Join/Interviews.sql
+++ Advanced Join/Interviews.sql
@@ -1,5 +1,5 @@
 /* Samantha interviews many candidates from different colleges using coding challenges and contests. 
-Write a query to print the contest_id, hacker_id, name, and the sums of total_submissions, 
+Write a query to print the contest_id, hacker_id, name, and sums of total_submissions, 
 total_accepted_submissions, total_views, and total_unique_views for each contest sorted by contest_id. 
 Exclude the contest from result if all four sums are 0. */
 
