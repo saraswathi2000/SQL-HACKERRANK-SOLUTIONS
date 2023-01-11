ef3c9d14b1c1bcf5dc201953eb303e32dddba48e
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2023-01-11 22:41:26 +0530

    Update Japan Population.sql

--- Aggregation/Japan Population.sql
+++ Aggregation/Japan Population.sql
@@ -8,7 +8,7 @@
 | District    | char(20) |
 | Population  | int(11)  |
 +-------------+----------+
-Query the sum of the populations for all the Japanese cities in CITY. The COUNTRYCODE for Japan is JPN. */
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN. */
 
 SELECT SUM(POPULATION) FROM CITY
 WHERE COUNTRYCODE ='JPN';
