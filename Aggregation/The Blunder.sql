0be93b64276222094b8d8c424168f6bfe8b8d381
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2023-01-12 14:37:08 +0530

    Update The Blunder.sql

--- Aggregation/The Blunder.sql
+++ Aggregation/The Blunder.sql
@@ -1,7 +1,7 @@
 /* Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, 
 but did not realize her keyboard's 0 key was broken until after completing the calculation. 
 She wants your help finding the difference between her miscalculation (using salaries with any zeroes removed), and the actual average salary.
-Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to next integer. */
+Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer. */
 
 SELECT CEIL(AVG(Salary)-AVG(REPLACE(Salary,'0','')))
 FROM  EMPLOYEES
