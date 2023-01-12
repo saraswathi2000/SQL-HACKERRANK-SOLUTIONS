ce9495644d5ed1151fe90f758835105ed35ebfa7
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2023-01-12 14:38:00 +0530

    Update Top Earners.sql

--- Aggregation/Top Earners.sql
+++ Aggregation/Top Earners.sql
@@ -7,7 +7,7 @@
 | months      | char(35) |
 | salary      | int(11)  |
 +-------------+----------+
-We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. 
+We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in Employee table. 
 Write a query to find the maximum total earnings for all employees as well as the total number of employees who have the maximum total earnings. 
 Then print these values as space-separated integers. */
 
