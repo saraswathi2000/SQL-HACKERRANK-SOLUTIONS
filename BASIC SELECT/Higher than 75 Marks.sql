0342fb1c5273893f980c07ce6966f515be712993
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2023-01-13 10:51:07 +0530

    Update Higher than 75 Marks.sql

--- BASIC SELECT/Higher than 75 Marks.sql
+++ BASIC SELECT/Higher than 75 Marks.sql
@@ -8,7 +8,7 @@
 | Marks       | int(3)   |
 +-------------+----------+
 
-Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name.
+Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by last three characters of each name.
 If two or more students both have names that are ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID. */
 
 SELECT NAME FROM STUDENTS 
