4dc8112836d859eb8dffe5fa4c34a6032dbad8f3
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2023-01-20 10:24:36 +0530

    Update Binary Tree Nodes.sql

--- ADVANCED SELECT/Binary Tree Nodes.sql
+++ ADVANCED SELECT/Binary Tree Nodes.sql
@@ -1,6 +1,6 @@
 /* You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
 Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
-Root: If node is root node.
+Root: If node is root nod.
 Leaf: If node is leaf node.
 Inner: If node is neither root nor leaf node. */
 
